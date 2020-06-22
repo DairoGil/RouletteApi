07cb579b49cffb10486d11e2610e89861fdd427b
Dairogil23 <dairo.gil@landsoft.com.co>
2020-06-22 15:50:15 -0500

    API creation.

---
+++ RouletteApi/Startup.cs
@@ -0,0 +1,52 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RouletteApi
+{
+    public class Startup
+    {
+
+        private static string DataBaseHost = Environment.GetEnvironmentVariable("DataBaseHost");
+        private static string DataBaseUser = Environment.GetEnvironmentVariable("DataBaseUser");
+        private static string DataBasePassword = Environment.GetEnvironmentVariable("DataBasePassword");
+        private static string DataBaseName = Environment.GetEnvironmentVariable("DataBaseName");
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
